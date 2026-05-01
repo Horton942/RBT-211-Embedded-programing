3eea7b42d6fcf62d11f35994fc5338cae0b49a4f
Horton942 <156350911+Horton942@users.noreply.github.com>
2026-05-01 05:50:05 -0500

    Add files via upload

--- BitVisualiser/main.c
+++ BitVisualiser/main.c
@@ -1,1 +1,47 @@
+/*
+ * potMeterV1.c
+ *
+ * Created: 4/8/2025 6:06:17 PM
+ * Author : Marcus
+ */ 
+
+#define F_CPU 16000000L
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+static inline void initADC0(void){
+	ADMUX |= (1 << REFS0);
+	ADCSRA |= (1 << ADPS2 | ADPS1 | ADPS0);
+	ADCSRA |= (1 << ADEN);
+}
+
+int main(void)
+{
+	// make variables
+	uint8_t ledValue;
+	uint16_t adcValue;
+	uint8_t i;
+	
+	//initialize adc
+	initADC0();
+	
+	//set D as Outputs
+	DDRD = 0xff;
+    while (1) 
+    {
+		//start ADC collection
+		ADCSRA |= (1 << ADSC);
+		loop_until_bit_is_clear(ADCSRA, ADSC);
+		adcValue = ADC;
+		ledValue = (adcValue >> 7);
+		
+		
+		PORTD = 0;
+		for (i = 0; i <= ledValue; i++) {
+			PORTD |= (1 << i);
+    }
+	_delay_ms(50);
+}
+}
 
